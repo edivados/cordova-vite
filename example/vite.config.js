eec1a5aaa5b97df038a75eed683db69d085f1bc7
Davide <43080019+edivados@users.noreply.github.com>
2024-11-02 22:56:35 +0100

    vite plugin only

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-import cordova from "cordova-plugin-vite";
-import solid from "vite-plugin-solid";
+import cordova from 'vite-plugin-cordova';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    hmr: {
+      host: '10.0.2.2'
+    }
+  },
   plugins: [
-    cordova(),
+    cordova("android"),
     solid()
   ]
 });
